613b9caf7c520d1f819877837c26dd69761adf09
Đồng Hữu Hải <huuhaihd21@gmail.com>
2026-06-11 18:58:47 +0700

    Create ProductList.jsx

---
+++ ProductList.jsx
@@ -0,0 +1,46 @@
+const plants = {
+"Aromatic Plants": [
+{ id: 1, name: "Lavender", price: 10 },
+{ id: 2, name: "Mint", price: 8 },
+{ id: 3, name: "Rosemary", price: 12 },
+{ id: 4, name: "Basil", price: 9 },
+{ id: 5, name: "Thyme", price: 7 },
+{ id: 6, name: "Sage", price: 11 }
+],
+
+"Medicinal Plants": [
+{ id: 7, name: "Aloe Vera", price: 15 },
+{ id: 8, name: "Neem", price: 14 },
+{ id: 9, name: "Tulsi", price: 10 },
+{ id: 10, name: "Chamomile", price: 13 },
+{ id: 11, name: "Echinacea", price: 16 },
+{ id: 12, name: "Ginseng", price: 18 }
+],
+
+"Decorative Plants": [
+{ id: 13, name: "Monstera", price: 25 },
+{ id: 14, name: "Snake Plant", price: 20 },
+{ id: 15, name: "Pothos", price: 18 },
+{ id: 16, name: "Peace Lily", price: 22 },
+{ id: 17, name: "ZZ Plant", price: 24 },
+{ id: 18, name: "Spider Plant", price: 17 }
+]
+};
+
+function ProductList() {
+return (
+
+
+Home
+Plants
+Cart (0)
+
+
+
+  {/* Display categories and products here */}
+</div>
+
+);
+}
+
+export default ProductList;
