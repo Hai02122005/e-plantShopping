93811220bd532a550324f3f03c80b842a10aeb47
Đồng Hữu Hải <huuhaihd21@gmail.com>
2026-06-11 18:57:12 +0700

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom";
+
+function App() {
+return (
+
+
+Paradise Nursery
+Your Online House Plant Store
+
+    <Link to="/products">
+      <button className="get-started-btn">
+        Get Started
+      </button>
+    </Link>
+  </div>
+</div>
+
+);
+}
+
+export default App;
